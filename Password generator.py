fb81c26c4e4c8789d28e7780b031d24595805385
shahil-sk <shahil8ahmed.sk@gmail.com>
2026-02-22 10:04:39 +0530

    Fix Randomization to make the password more complex

--- Password generator.py
+++ Password generator.py
@@ -6,16 +6,38 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+
+# This is where we'll store all the characters
+
+password_char = []
   
 
 print("Welcome to the PyPassword Generator!")
 
 nr_letters = int(input("How many letters would you like in your password?\n"))
 
-nr_symbols = int(input("How many symbols would you like?\n"))
+for _ in range(nr_letters):
+    password_char.append(random.choice(letters))
 
 nr_numbers = int(input("How many numbers would you like?\n"))
 
-  
+for _ in range(nr_numbers):
+    password_char.append(random.choice(numbers))
 
-password=random.choice(letters)
+nr_symbols = int(input("How many symbols would you like?\n"))
+
+for _ in range(nr_symbols):
+    password_char.append(random.choice(symbols))
+
+# we'll shuffle here to avoid "ABD123!@#" pattern
+
+password = random.shuffle(password_char)
+
+# converting the $password_char to string
+
+password = "".join(password_char)
+
+# Your are not even using all the variables here azeem bro
+# password=random.choice(letters)
+
+print(password)
